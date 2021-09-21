75f14b6373f7fbca1fe2a7742c7745f67ea7ea74
Umair Khan <umairk83@live.com>
2021-09-21 19:14:05 +0500

    Event bus added

---
+++ src/BuildingBlock/Events/Events/Events/IntegrationBusEvent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+	public class IntegrationBusEvent
+	{
+		public IntegrationBusEvent()
+		{
+			Id = Guid.NewGuid();
+			CreationDate = DateTime.UtcNow;
+		}
+		public Guid Id { get; private set; }
+
+		public DateTime CreationDate { get; private set; }
+	}
+}
